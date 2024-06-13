698f9cc6af03038db3dc747521865c8e45822e6f
SCU Imaginarium (Aargorn) <92197005+crism123dm@users.noreply.github.com>
2024-06-12 18:02:06 -0700

    door open animation (living hell)

--- Assets/scripts/doorAnimationScript.cs
+++ Assets/scripts/doorAnimationScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playsound1 : MonoBehaviour
 {
+    public Animator myAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,10 @@ public class playsound1 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OldCupGrabbed()
+    {
+        myAnimator.SetBool("startAnimation", true);
     }
 }
